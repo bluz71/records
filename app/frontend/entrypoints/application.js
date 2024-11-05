bbc173365d08d9a026234953a8a9747e38c976ed
Dennis B <bluz71@users.noreply.github.com>
2024-11-05 15:14:20 +1100

    Only use JS files in this project (not TS)

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,8 +1,8 @@
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs"
 import focus from "@alpinejs/focus"
 
 Alpine.plugin(focus)
-window.Alpine = Alpine;
-Alpine.start();
+window.Alpine = Alpine
+Alpine.start()
 
-console.log('Vite ⚡️ Rails')
+console.log("Vite ⚡️ Rails")
